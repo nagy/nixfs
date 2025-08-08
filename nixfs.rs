f0f7b9b4893d07c4d031241dd0c4ee3439f5ee42
Daniel Nagy <danielnagy@posteo.de>
2025-08-08 10:45:00 +0200

    Use memoize crate

--- nixfs.rs
+++ nixfs.rs
@@ -1,15 +1,16 @@
 use fuser::{
-    FileAttr, FileType, Filesystem, MountOption, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry,
-    Request,
+    FileAttr, FileType, Filesystem, MountOption, ReplyAttr, ReplyData, ReplyEntry, Request,
 };
 
 use libc::ENOENT;
+use memoize::memoize;
 use std::collections::HashMap;
 use std::ffi::OsStr;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use std::process::Command;
 use std::time::{Duration, UNIX_EPOCH};
 
-const TTL: Duration = Duration::from_secs(30);
+const TTL: Duration = Duration::from_secs(1);
 
 fn make_symlink_attr(inode: u64) -> FileAttr {
     FileAttr {
@@ -33,45 +34,50 @@ fn make_symlink_attr(inode: u64) -> FileAttr {
 
 #[derive(Default)]
 struct HelloFS {
-    hashmap: HashMap<(u64, String), String>,
-    counter: u64,
+    hashmap: HashMap<u64, String>,
+}
+
+#[memoize]
+fn nix_attr_to_outpath(attr: String) -> String {
+    eprintln!("execute: {:?}", attr);
+    let output = Command::new("nix-build")
+        .arg("--no-out-link")
+        .arg("<nixpkgs>")
+        .arg("-A")
+        .arg(attr)
+        .output()
+        .unwrap();
+    let stdout: String = String::from_utf8(output.stdout)
+        .unwrap()
+        .strip_suffix("\n")
+        .unwrap()
+        .to_string();
+    stdout
 }
 
 impl Filesystem for HelloFS {
     fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
-        eprintln!("Just some lookup: {:?}", name);
-        if parent == 1 {
-            if name.to_str().unwrap_or("").starts_with("_eval") {
-                let attr = name.to_str().unwrap().strip_prefix("_eval").unwrap();
-                eprintln!("inserting, Just calc: {:?}", attr);
-                let output = Command::new("nix-build")
-                    .arg("--no-out-link")
-                    .arg("<nixpkgs>")
-                    .arg("-A")
-                    .arg(attr)
-                    .output()
-                    .unwrap();
-                let stdout: String = String::from_utf8(output.stdout)
-                    .unwrap()
-                    .strip_suffix("\n")
-                    .unwrap()
-                    .to_string();
-
-                eprintln!("result: {:?}", stdout);
-                self.hashmap
-                    .insert((self.counter, attr.to_string()), stdout);
-                self.counter += 1;
-                eprintln!("result2: {:?}", self.hashmap);
-            }
-
-            for (key, _) in self.hashmap.iter() {
-                if &key.1 == name.to_str().unwrap_or("") {
-                    reply.entry(&TTL, &make_symlink_attr(key.0), 0);
-                    return;
-                }
-            }
+        eprintln!("Lookup: {:?}", name);
+        MEMOIZED_MAPPING_NIX_ATTR_TO_OUTPATH.with_borrow(|v| {
+            eprintln!("storeident:: {:?}", v);
+        });
+        if parent != 1 {
+            reply.error(ENOENT);
+            return;
         }
-        reply.error(ENOENT);
+        if !name.to_str().unwrap_or("").starts_with("_eval") {
+            reply.error(ENOENT);
+            return;
+        }
+        let attr = name.to_str().unwrap().strip_prefix("_eval").unwrap();
+        eprintln!("inserting attr: {:?}", attr);
+        let hashinode = {
+            let mut hasher = DefaultHasher::new();
+            attr.hash(&mut hasher);
+            hasher.finish()
+        };
+        reply.entry(&TTL, &make_symlink_attr(hashinode), 0);
+        self.hashmap.insert(hashinode, attr.to_string());
     }
 
     fn getattr(&mut self, _req: &Request, ino: u64, _fh: Option<u64>, reply: ReplyAttr) {
@@ -92,20 +98,23 @@ impl Filesystem for HelloFS {
             flags: 0,
             blksize: 512,
         };
-        match ino {
-            1 => reply.attr(&TTL, &HELLO_DIR_ATTR),
-            // 2 => reply.attr(&TTL, &HELLO_TXT_ATTR),
-            3 => reply.attr(&TTL, &make_symlink_attr(3)),
-            _ => reply.error(ENOENT),
+        /* parent */
+        if ino == 1 {
+            reply.attr(&TTL, &HELLO_DIR_ATTR);
+            return;
         }
+        if let Some(_) = self.hashmap.get(&ino) {
+            reply.attr(&TTL, &make_symlink_attr(ino));
+            return;
+        }
+        reply.error(ENOENT);
     }
 
     fn readlink(&mut self, _req: &Request, inode: u64, reply: ReplyData) {
-        for (key, path) in self.hashmap.iter() {
-            if key.0 == inode {
-                reply.data(path.as_bytes());
-                return;
-            }
+        if let Some(found) = self.hashmap.get(&inode) {
+            let found2 = nix_attr_to_outpath(found.to_string());
+            reply.data(found2.as_bytes());
+            return;
         }
         reply.error(ENOENT);
     }
@@ -116,25 +125,16 @@ impl Filesystem for HelloFS {
         ino: u64,
         _fh: u64,
         offset: i64,
-        mut reply: ReplyDirectory,
+        mut reply: fuser::ReplyDirectory,
     ) {
         if ino != 1 {
             reply.error(ENOENT);
             return;
         }
-
-        let mut entries: Vec<(u64, FileType, String)> = vec![
-            // let mut entries = vec![
-            (1, FileType::Directory, ".".to_string()),
-            (1, FileType::Directory, "..".to_string()),
-            // (2, FileType::RegularFile, "hello.txt"),
-            // (3, FileType::Symlink, "python3Hy"),
+        let entries = vec![
+            (1, FileType::Directory, "."),
+            (1, FileType::Directory, ".."),
         ];
-        for (key, _) in self.hashmap.iter() {
-            let val = format!("_eval{}", key.1);
-            entries.push((key.0, FileType::Symlink, val.to_string()));
-        }
-
         for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
             // i + 1 means the index of the next entry
             if reply.add(entry.0, (i + 1) as i64, entry.1, entry.2) {
